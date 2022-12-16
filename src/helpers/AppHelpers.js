d64ca8a5a0a95ef97a0723d816567dfa9e3a5a65
Rully Ihza Mahendra <rullyihza00@gmail.com>
2022-12-17 01:59:07 +0700

    Updated AppHelper.js

--- src/helpers/AppHelpers.js
+++ src/helpers/AppHelpers.js
@@ -1,11 +1,19 @@
-const configuration = {
-  apiKey: '',
-  titleApp: 'Document'
+const _configuration = {
+  name: 'Document',
+  title: 'Document'
 };
 
-export const initApp = ({title = 'Document'}) => {
-  document.title = title
-  configuration.titleApp = title
+export const initApp = ({name = 'Document', title = ''}) => {
+  const documentTitle = 
+    (typeof(title) !== 'string' || title.length <= 0) ? name : title;
+
+  setTitle(documentTitle);
+  _configuration.name = name;
 };
 
-export const appConfiguration = () => configuration;
+export const setTitle = (newTitle) => {
+  _configuration.title = newTitle;
+  document.title = newTitle;
+}
+
+export const appConfiguration = () => _configuration;
