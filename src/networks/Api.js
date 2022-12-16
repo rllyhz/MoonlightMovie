6dba66f1fe7047599a594c7760513f1b323d054e
Rully Ihza Mahendra <rullyihza00@gmail.com>
2022-12-17 03:59:12 +0700

    Added image url on Api.js

--- src/networks/Api.js
+++ src/networks/Api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const baseUrl = 'https://api.themoviedb.org/3/';
+export const imageBaseUrl = 'https://image.tmdb.org/t/p/original/';
 const apiKey = 'b3201e20a048f23eb585e0df35d61409';
 
 const api = axios.create({
