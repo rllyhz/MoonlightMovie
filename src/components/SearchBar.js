f3d17b520083f640fbe7d17e6c995c039cc792b9
Rully Ihza Mahendra <rullyihza00@gmail.com>
2022-12-17 02:57:29 +0700

    Created SearchBar.js

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,12 +1,25 @@
 export default class SearchBar extends HTMLElement {
   static tagName = 'search-bar'
 
-  connectedCallback() {
+  set searchData(newData) {
+    this._data = newData;
     this.render();
   }
 
   render() {
-    this.innerHTML = ``;
+    const {type, placeholderText, submitText, onSubmitCallback} = this._data;
+
+    this.innerHTML = `
+      <div class='search'>
+        <input class='search-input' type='${type}' placeholder='${placeholderText}' />
+        <button class='search-button' type='button'>${submitText}</button>
+      </div>
+    `;
+
+    this.querySelector('.search-button').addEventListener('click', (e) => {
+      const currentValue = this.querySelector('.search-input').value;
+      onSubmitCallback(currentValue, e.target);
+    });
   }
 }
 
