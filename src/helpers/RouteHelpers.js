e9b3f9c09c3966b1916902edbfd533fa44b23dd3
Rully Ihza Mahendra <rullyihza00@gmail.com>
2022-12-17 08:26:50 +0700

    Created Router logics

---
+++ src/helpers/RouteHelpers.js
@@ -0,0 +1,59 @@
+let reloadCallback = null;
+let preReloadCallback = null;
+const history = [];
+
+export const PATH = {
+  home: 0,
+  search: 1
+};
+
+export class Router {
+
+  static onPreReload(callback) {
+    preReloadCallback = callback;
+  }
+
+  static onReload(callback) {
+    reloadCallback = callback;
+  }
+
+  static load(initialPath) {
+    if (history.length <= 0) {
+      history.push(initialPath);
+    }
+
+    if (preReloadCallback != null && typeof(preReloadCallback) == 'function') {
+      preReloadCallback();
+    }
+
+    if (reloadCallback != null && typeof(reloadCallback) == 'function') {
+      reloadCallback(history[0], null);
+    }
+  }
+
+  static navigateTo(path, data = null) {
+    history.unshift(path);
+
+    if (preReloadCallback != null && typeof(preReloadCallback) == 'function') {
+      preReloadCallback();
+    }
+
+    if (reloadCallback != null && typeof(reloadCallback) == 'function') {
+      reloadCallback(history[0], data);
+    }
+  }
+
+  static navigateUp() {
+    if (history.length > 1) {
+      history.shift();
+    }
+
+    if (preReloadCallback != null && typeof(preReloadCallback) == 'function') {
+      preReloadCallback();
+    }
+
+    if (reloadCallback != null && typeof(reloadCallback) == 'function') {
+      reloadCallback(history[0], null);
+    }
+  }
+}
