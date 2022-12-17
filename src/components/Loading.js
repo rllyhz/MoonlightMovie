d84094520c6eda1405894a1eadd62557ded16934
Rully Ihza Mahendra <rullyihza00@gmail.com>
2022-12-17 08:40:03 +0700

    Created loading ui

---
+++ src/components/Loading.js
@@ -0,0 +1,17 @@
+export default class Loading extends HTMLElement {
+  static tagName = 'loading-ui'
+
+  connectedCallback() {
+    this.render();
+  }
+
+  render() {
+    this.innerHTML = `
+      <div class='loading-wrapper'>
+        <div class='loading'></div>
+      <div>
+    `;
+  }
+}
+
+customElements.define(Loading.tagName, Loading);
