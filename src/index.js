86c141a0b137bf2312cf06abb5220f8f85db188b
Rully Ihza Mahendra <rullyihza00@gmail.com>
2022-12-19 01:47:44 +0700

    Finished up

--- src/index.js
+++ src/index.js
@@ -12,72 +12,74 @@ import SearchResultPage from './pages/SearchResultPage';
 import MovieDetailPage from './pages/MovieDetailPage';
 import swal from 'sweetalert';
 
-initApp({ name: 'MoonlightMovie' });
+window.addEventListener('DOMContentLoaded', _ => {
+  initApp({ name: 'MoonlightMovie' });
 
-// TopBar
-appendBody(
-  createElement({
-    tagName: TopBar.tagName,
-    id: 'my-top-bar',
-    data: {
-      config: {
-        title: appConfiguration().name,
-        shouldNavigateUpShown: false,
-        onBackPressed: () => { Router.navigateUp() }
+  // TopBar
+  appendBody(
+    createElement({
+      tagName: TopBar.tagName,
+      id: 'my-top-bar',
+      data: {
+        config: {
+          title: appConfiguration().name,
+          shouldNavigateUpShown: false,
+          onBackPressed: () => { Router.navigateUp() }
+        }
       }
-    }
-  })
-);
+    })
+  );
 
-// SearchBar
-appendBody(
-  createElement({
-    tagName: SearchBar.tagName,
-    id: 'my-search-bar',
-    data: {
-      searchData: {
-        type: 'text',
-        placeholderText: 'Search here (Ex. Avatar)',
-        submitText: 'Search',
-        onSubmitCallback: (value) => { 
-          if (value.length > 0) {
-            Router.navigateTo(PATH.search, value);
-          } else {
-            if (Router.getActivePath() == PATH.search) {
-              Router.navigateUp();
+  // SearchBar
+  appendBody(
+    createElement({
+      tagName: SearchBar.tagName,
+      id: 'my-search-bar',
+      data: {
+        searchData: {
+          type: 'text',
+          placeholderText: 'Search here (Ex. Avatar)',
+          submitText: 'Search',
+          onSubmitCallback: (value) => { 
+            if (value.length > 0) {
+              Router.navigateTo(PATH.search, value);
             } else {
-              swal('Searching Failed','Please insert your query!', 'warning');
+              if (Router.getActivePath() == PATH.search) {
+                Router.navigateUp();
+              } else {
+                swal('Searching Failed','Please insert your query!', 'warning');
+              }
             }
           }
         }
       }
+    })
+  );
+
+  appendBody(
+    createElement({
+      tagName: 'div',
+      classNames: 'container-app'
+    })
+  );
+
+  // Router init
+  Router.onPreReload(() => {
+    getElem('.container-app').innerHTML = '';
+  });
+
+  Router.onReload((currentPath, data) => {
+    if (currentPath == PATH.home) {
+      HomePage();
+    } else if (currentPath == PATH.search) {
+      SearchResultPage(data);
+    } else if (currentPath == PATH.detail) {
+      MovieDetailPage(data);
+    } else {
+      swal('Opps...','Page not found!', 'error');
+      Router.navigateTo(PATH.home);
     }
-  })
-);
+  });
 
-appendBody(
-  createElement({
-    tagName: 'div',
-    classNames: 'container-app'
-  })
-);
-
-// Router init
-Router.onPreReload(() => {
-  getElem('.container-app').innerHTML = '';
+  Router.load({ initialPath: PATH.home });
 });
-
-Router.onReload((currentPath, data) => {
-  if (currentPath == PATH.home) {
-    HomePage();
-  } else if (currentPath == PATH.search) {
-    SearchResultPage(data);
-  } else if (currentPath == PATH.detail) {
-    MovieDetailPage(data);
-  } else {
-    swal('Opps...','Page not found!', 'error');
-    Router.navigateTo(PATH.home);
-  }
-});
-
-Router.load({ initialPath: PATH.home });
